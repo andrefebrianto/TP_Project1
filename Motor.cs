c6a662d7719e40bfe4fa5be852a3937127411a71
andrefebrianto <andrefebrianto88@yahoo.co.id>
2015-06-11 03:42:22 +0700

    Menambahkan Class

---
+++ Motor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Motor
+{
+    public string Merk { get; set; }
+    public double HargaTunai { get; set; }
+    public double Cicilan { get; set; }
+    public double DownPayment { get; set; }
+	public Motor()
+	{
+
+	}
+    public Motor(string merk, string tunai)
+    {
+        Merk = merk;
+        HargaTunai = tunai;
+        Cicilan = 0;
+        DownPayment = 0;
+    }
+    public Motor(string merk, string tunai, string cicilan, string dp)
+    {
+        Merk = merk;
+        HargaTunai = tunai;
+        Cicilan = cicilan;
+        DownPayment = dp;
+    }
+    public double HargaTotalCicilan()
+    {
+
+    }
+}
---
+++ Pembeli.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Pembeli
+{
+    public string Nama { get; set; }
+    public string Alamat { get; set; }
+    public Motor Barang { get; set; }
+
+	public Pembeli()
+	{
+	}
+}
